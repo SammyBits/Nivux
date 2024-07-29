3e202b5f269a4cc03f57ba76af6358cbb2d0e74d
SammyBits <samuelbeato7@gmail.com>
2024-07-29 01:15:39 -0400

    doc: Describe MainLayout

--- src/layouts/public/MainLayout.tsx
+++ src/layouts/public/MainLayout.tsx
@@ -1,5 +1,8 @@
 import { Outlet } from "react-router-dom";
-
+/**
+ * MainLayout Este componente es el principal, se encarga de mostrar el contenido de las rutas publicas con el mismo estilo
+ * @returns 
+ */
 export const MainLayout = () => {
     return (
         <div className="h-screen bg-[#111827] grid place-content-center">
